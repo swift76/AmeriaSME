bf940f8a0fe6fa571aab8999e32846543f723203
Narek Sahakyan <nareksahakyan26@gmail.com>
2022-04-21 17:02:57 +0400

    add eslint, remove tslint

--- backend/Scoring/IntelART.OnlineLoans.ShopModuleWebApp/ClientApp/src/app/containers/pageContainers/Authorization.tsx
+++ backend/Scoring/IntelART.OnlineLoans.ShopModuleWebApp/ClientApp/src/app/containers/pageContainers/Authorization.tsx
@@ -1,83 +1,86 @@
 import * as AppTypes from 'AppTypes'
 
-import {AppContext, IAppContext} from 'app/AppContext'
-import {RouteComponentProps, withRouter} from 'react-router';
+import { AppContext, IAppContext } from 'app/AppContext'
+import { RouteComponentProps, withRouter } from 'react-router'
 
-import React from 'react';
+import React from 'react'
 import { connect } from 'react-redux'
 import { matchRoutes } from 'react-router-config'
 
 class Authorization extends React.Component<IProps, IState> {
+  static contextType = AppContext
 
-    public static contextType = AppContext
+  static getDerivedStateFromProps(props: IProps, state: IState) {
+    const { location, user } = props
+    const { userLoggedIn } = user
+    const matched = matchRoutes(state.routes, location.pathname)[0]
 
-    public static getDerivedStateFromProps(props: IProps, state: IState) {
-        const { location, user } = props
-        const {userLoggedIn} = user
-        const matched = matchRoutes(state.routes, location.pathname)[0]
+    const hasRouteAccess =
+      matched && matched.route.auth
+        ? userLoggedIn
+        : userLoggedIn && !matched.route.auth
+        ? false
+        : true
 
+    return { hasRouteAccess }
+  }
 
-        const hasRouteAccess = matched && matched.route.auth ?
-        userLoggedIn : userLoggedIn && !matched.route.auth ? false : true
-
-
-        return { hasRouteAccess }
+  constructor(props: IProps, context: IAppContext) {
+    super(props)
+    const { routes } = context
+    this.state = {
+      hasRouteAccess: true,
+      routes,
     }
+  }
 
-    constructor(props: IProps, context: IAppContext) {
-        super(props);
-        const { routes } = context
-        this.state = {
-            hasRouteAccess: true,
-            routes
-        };
+  componentDidMount() {
+    if (!this.state.hasRouteAccess) {
+      this.redirectRoute(this.props)
     }
+  }
 
-    public componentDidMount() {
-        if (!this.state.hasRouteAccess) {
-            this.redirectRoute(this.props)
-        }
+  componentDidUpdate() {
+    if (!this.state.hasRouteAccess) {
+      this.redirectRoute(this.props)
     }
+  }
 
-    public componentDidUpdate() {
-        if (!this.state.hasRouteAccess) {
-          this.redirectRoute(this.props)
-        }
+  shouldComponentUpdate(nextProps: IProps, nextState: IState) {
+    return nextState.hasRouteAccess !== this.state.hasRouteAccess
+  }
+
+  redirectRoute(props: IProps) {
+    const { location, user, history } = props
+    const { pathname, state } = location
+
+    if (!user.userLoggedIn) {
+      history.push({
+        pathname: '/login',
+        state: { redirectUrl: pathname },
+      })
+    } else {
+      // const redirectUrl = state && state.redirectUrl ? state.redirectUrl : '/'
+
+      history.push('/')
     }
+  }
 
-    public shouldComponentUpdate(nextProps: IProps, nextState: IState) {
-        return nextState.hasRouteAccess !== this.state.hasRouteAccess
-    }
-
-    public redirectRoute(props: IProps) {
-        const { location, user, history } = props
-        const { pathname, state } = location
-
-        if (!user.userLoggedIn) {
-          history.push({
-            pathname: '/login',
-            state: { redirectUrl: pathname },
-          })
-        } else {
-            // const redirectUrl = state && state.redirectUrl ? state.redirectUrl : '/'
-
-            history.push('/')
-        }
-    }
-
-    public render() {
-        return this.state.hasRouteAccess ? <React.Fragment>{this.props.children}</React.Fragment> : null
-    }
+  render() {
+    return this.state.hasRouteAccess ? (
+      <React.Fragment>{this.props.children}</React.Fragment>
+    ) : null
+  }
 }
 
 const mapStateToProps = (state: AppTypes.ReducerState) => ({
-    user: state.user
+  user: state.user,
 })
 
 type IProps = ReturnType<typeof mapStateToProps> & RouteComponentProps
 
-interface IState extends IAppContext{
-    hasRouteAccess: boolean,
+interface IState extends IAppContext {
+  hasRouteAccess: boolean;
 }
 
 export default withRouter(connect(mapStateToProps)(Authorization))
